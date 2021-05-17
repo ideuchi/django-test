9b71703fbefc34556f5255e7799c13978ec63137
Ideuchi Masao <mideuchi@mastar.nict.go.jp>
2021-05-17 13:01:59 +0900

    Django REST Framework debug test

--- setup.py
+++ setup.py
@@ -1,1 +1,7 @@
-print('setup.py called')
+import datetime
+
+time = datetime.datetime.now()
+str_time = time.strftime('%Y/%m/%d %H:%M:%S')
+
+with open('debug.txt', 'a') as f:
+    print(str_time+': setup.py called', file=f)
