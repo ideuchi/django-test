46f32fd8043188752cb3eaf93df2e930cdc8f036
Ideuchi Masao <mideuchi@mastar.nict.go.jp>
2021-05-17 20:21:15 +0900

    Django REST Framework debug test

--- debug_message_file/views.py
+++ debug_message_file/views.py
@@ -8,20 +8,18 @@ import subprocess as sp
 def index(request):
     time = datetime.datetime.now()
     str_time = time.strftime('%Y/%m/%d %H:%M:%S')
-    debug_message = str_time+': debug called.\n'
+    debug_message = str_time+' debug called.\n'
     if os.path.isfile('debug.txt'):
         with open('debug.txt') as f1:
             debug_message += f1.read()
-    with open('debug.txt', 'a') as f:
-        print(str_time+': debug called.\n', file=f)
     return HttpResponse('<pre>' + debug_message + '</pre>')
 
 def files(request):
     time = datetime.datetime.now()
     str_time = time.strftime('%Y/%m/%d %H:%M:%S')
-    message = str_time + '\n'
+    message = str_time + '/files called.\n'
     message += 'current dir: ' + os.getcwd() + '\n'
-    message += '__file__:    ' + __file__ + '\n'
+    message += '__file__:    ' + __file__) + '\n'
     message += 'basename:    ' + os.path.basename(__file__) + '\n'
     message += 'dirname:     ' + os.path.dirname(__file__) + '\n'
     cmd = 'ls -l'
@@ -30,22 +28,22 @@ def files(request):
     ls_file_name = std_out.decode('utf-8').rstrip()
     message += 'ls -l result: ' + '\n' + ls_file_name
     with open('debug.txt', 'a') as f:
-        print(message, file=f)
+        print('\n/files stdout:\n' + message, file=f)
     return HttpResponse('<pre>' + message + '</pre>')
 
 def prepare_debug(request):
     time = datetime.datetime.now()
     str_time = time.strftime('%Y/%m/%d %H:%M:%S')
-    message = str_time + '\n'
+    message = str_time + '/prepare_debug called.\n'
     cmd = 'chmod +x debug_message_file/setup_test'
     proc= sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     std_out, std_err = proc.communicate()
-    message += cmd + ' result: \n  std_out:\n' + std_out.decode('utf-8').rstrip() + '\n  std_err:\n' + std_err.decode('utf-8').rstrip() + '\n'
+    message += cmd + ' result: \n  ' + cmd + ' std_out:\n' + std_out.decode('utf-8').rstrip() + '\n  ' + cmd + ' std_err:\n' + std_err.decode('utf-8').rstrip() + '\n'
     cmd = 'debug_message_file/setup_test'
     proc= sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     std_out, std_err = proc.communicate()
-    message += cmd + ' result: \n  std_out:\n' + std_out.decode('utf-8').rstrip() + '\n  std_err:\n' + std_err.decode('utf-8').rstrip() + '\n'
+    message += cmd + ' result: \n  ' + cmd + ' std_out:\n' + std_out.decode('utf-8').rstrip() + '\n  ' + cmd + ' std_err:\n' + std_err.decode('utf-8').rstrip() + '\n'
     with open('debug.txt', 'a') as f:
-        print(message, file=f)
+        print('\n' + message, file=f)
     return HttpResponse('<pre>' + message + '</pre>')
 
